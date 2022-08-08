41545349e0697923cce3c10873a2061ddf3357cd
Edward F <38357638+edwardfue@users.noreply.github.com>
2022-08-08 16:29:41 -0400

    Add files via upload
    Is the solution complete?
    
    [Yes]
    
    Did you run into any problems?
    
    [Describe...] the only problem that I faced was how to run the code while still learning to run the code also how to upload it  
    
    Did you collaborate with others on this solution?
    No, check on the recording and check on websites like geeks for geeks for any ideas for doing the code work.

---
+++ validators.js
@@ -0,0 +1,56 @@
+let letter = [A-za-z];
+let number = [0-9];
+var specialCharacters = "~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
+/*
+  Write a function that returns true or false if the given username
+  is valid according to the following rules:
+  - username must be between (and including) 3-10 characters in length
+  - username must begin with a letter
+  - username may contain numbers and letters
+  - username cannot contain special characters
+*/
+function validUsername(username) {
+  if((username.lenght >= 3 ) || (username.lenght <= 10)){
+    return true;
+  }
+
+  if(username.lenght === 1 && username.match(letter) ){
+    return true; 
+  }
+
+  if((username === letter) && (username === number)){
+    return true;
+  }
+
+  if(username != specialCharacters){
+    return true;
+  }
+
+  else{
+    return false;
+  }
+
+  return;
+}
+
+/*
+  Write a function that returns true or false if the given password
+  is valid according to the following rules:
+  - password must be between (and including) 10-64 characters in length
+  - password must contain at least 1 letter, 1 number, and 1 special character
+*/
+function validPassword(password) {
+  if((password.lenght >= 10) && (password.lenght <= 64)){
+    return true;
+  }
+  if((password === letter) && (password === number) && (password === specialCharacters)){
+    return true
+  }
+  else{
+    return false;
+  }
+
+  return;
+}
+
+module.exports = { validUsername, validPassword };
