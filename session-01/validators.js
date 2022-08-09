0ff1eb304a667db569b2f97fbdd0bdc0892f9f61
VincenzoCammilleri2 <cammilleriv2@gmail.com>
2022-08-09 14:07:56 -0400

    fifth commit

--- session-01/validators.js
+++ session-01/validators.js
@@ -148,7 +148,7 @@ function validUsername(username) {
   {
   console.log("Password must be AT MOST 64 characters long!");
   return false;
-  }else if(password !== 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' || password != '0','1','2','3','4','5','6','7','8','9' || password!=='!','@','#','$','%','^','&','*','(',')','/','<','>','?','{','}','[',']',';',':','"','|')
+  }else if(hasLetterSpecCharNumber(password))
     return true;
   }
   
