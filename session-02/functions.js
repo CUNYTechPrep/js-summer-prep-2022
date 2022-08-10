0e1eaba92342f195605ed22d4f961cade031adf2
Edgardo Molina <medgardo@gmail.com>
2022-08-10 11:35:46 -0400

    Session 2 notes

--- session-02/functions.js
+++ session-02/functions.js
@@ -10,14 +10,17 @@ const add2 = function (a, b) {
   return a + b;
 };
 
-// Arrow functions
+// Arrow functions (ES6)
 const add3 = (a, b) => {
   return a + b;
 };
 
 // implicit return with single expression functions
-// (no curly braces {})
+// (no curly braces {}, and no return statement)
 const add4 = (a, b) => a + b;
+
+console.log(add4(3, 4));
+console.log(add4);
 
 // single parameter arrow function
 // parentheses are optional
