c38471cd917d9ff385abb7200ba5cfdb17ea6bef
TahminaM <Tahminamunni07@gmail.com>
2022-08-15 19:15:02 -0400

    Updated

--- session-01/validators.js
+++ session-01/validators.js
@@ -1,5 +1,4 @@
-/* 1.
-
+/* 
   Write a function that returns true or false if the given username
   is valid according to the following rules:
   - username must be between (and including) 3-10 characters in length
@@ -48,9 +47,7 @@ function validUsername(username){
 
 
 
-
-/* 2. 
-
+/* 
   Write a function that returns true or false if the given password
   is valid according to the following rules:
   - password must be between (and including) 10-64 characters in length
@@ -62,27 +59,40 @@ function validPassword(password) {       //password must be between (and includi
 
 
 
+let numberCount = 0;             //password must contain at least 1 number
+for (let i=0; i<password.length; i++){
+  let letter = password[i];
+  if(letter >= "0" && letter<= "9")  numberCount++;
+}
 
+if (numberCount === 0) return false;
+
+
+
+
+let alphaCount = 0;             //password must contain at least 1 letter
+for (let i=0; i<password.length; i++){
+  let letter = password[i];
+  if((letter >= "a" && letter<= "z") || (letter >= "A" && letter<= "Z"))  alphaCount++;
+}
+
+if (alphaCount === 0) return false;
+
+
+
+const specialPrintAbleCharacters = "  !\"#$%&'()*+,-./:;<=>?@{\\]^_`{|}~ "
+let specialCount = 0;             //password must contain at least 1 special character
+for (let i=0; i<password.length; i++){
+  let letter = password[i];
+  if(specialPrintAbleCharacters.includes(letter))  specialCount++;
+}
+
+if (specialCount === 0) return false;
 
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = { validUsername, validPassword };
