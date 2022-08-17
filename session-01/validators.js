cdc2ee23d2e7a194946940f8ffcbeea302c0c4e2
Tania Sacoto <taniasacoto@Tanias-MacBook-Pro.local>
2022-08-17 13:23:41 -0400

    [submission]

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,26 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+    if(username.length < 3 || username.length > 10) {
+      return false;
+    }
+    let lowercase = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    for(let i = 0; i < lowercase.length; i++){
+        if(username[0] !== lowercase[i])return false;
+    }
+    let uppercase = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    for(let j = 0; j < uppercase.length; j++){
+        if(username[0] !== uppercase[j])return false;
+    }
+    let s = 0;
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    for(let k = 0; k < username.length; k++){
+      if(username[k] !== lowercase[s] || username[k] !== uppercase[s]|| username[k] !== numbers[s]){
+        s++;
+      }
+      return false;
+    }
+  return true;
 }
 
 /*
@@ -17,7 +36,42 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+  if(password.length < 10 || password.length > 64){
+    return false;
+  }
+  let lowercase = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let uppercase = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  let specialCharacters = ["~","`","!", "@","#","$","%","^","&", "*","("," )","_","-","+","=","{","[","}","]","|",':' , ';' , "'" , '<' , '>' , '.' , '?' , '/'];
+  
+  let letterCount = 0;
+  let specialCharCount = 0;
+  let numCount = 0;
+  for(let a = 0; a < password.length; a++){
+    let b = 0;
+    if(password[a] === lowercase[b] || password[a] === uppercase[b]){
+      letterCount++;
+    }
+    b++;
+  } 
+  for(let c = 0; c < password.length; c++){
+    let d = 0;
+    if(password[c] === numbers[d]){
+      numCount++;
+    }
+    d++;
+  }
+  for(let e = 0; e < password.length; e++){
+    let f = 0;
+    if(password[e] === numbers[f]){
+      specialCharCount++;
+    }
+    f++;
+  } 
+  if(letterCount === 0 || numCount=== 0 || specialCharCount === 0){
+    return false;
+  }
+  return true;
 }
 
 module.exports = { validUsername, validPassword };
